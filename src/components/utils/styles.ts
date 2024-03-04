79e8e0fbf02802db68b8766aa400e599df383f7f
Antonio Zanotti <antoniorzanotti@gmail.com>
2024-03-04 18:10:39 -0300

    fix: improved link styles

--- src/components/utils/styles.ts
+++ src/components/utils/styles.ts
@@ -1,5 +1,5 @@
 const iconSize = "w-[38px] h-[38px] lg:w-[48px] lg:h-[48px]";
-const aHover = "hover:underline text-accent-2 dark:text-dark-accent-2";
+const aHover = "underline text-accent-2 dark:text-dark-accent-2 hover:text-secondary hover:dark:text-dark-secondary";
 
 export const styles = {
   iconSize: iconSize,
