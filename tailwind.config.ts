40625abf1673fa5153bbcdf8f3538916a5e90ea5
Antonio Zanotti <antoniorzanotti@gmail.com>
2024-03-06 11:45:08 -0300

    refactor: changed folder structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import presetTopazReact from "topaz-react/src/preset/tailwind-preset-topaz-react
 
 const config: Config = {
   content: [
-    "./node_modules/topaz-react/src/components/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/topaz-react/src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   presets: [presetTopazReact],
