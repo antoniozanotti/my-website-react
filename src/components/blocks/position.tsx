a73dde13c4aa04e400e9f18c7ba6b191b4f82109
Antonio Zanotti <antoniorzanotti@gmail.com>
2024-03-19 14:58:23 -0300

    fix: motion.div

--- src/components/blocks/position.tsx
+++ src/components/blocks/position.tsx
@@ -40,66 +40,68 @@ export default function Position({
   );
 
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} {...props}>
-      <Heading level="h3" title={title} />
-      <Grid cols={3} className="mt-4 space-y-4">
-        <div className="grid grid-flow-row auto-rows-max">
-          <span>{companyName}</span>
-          <span className="italic text-c2 lg:text-c3">{location}</span>
-          <span className="font-bold py-4">{period}</span>
-          {skills && (
-            <div className="flex flex-wrap gap-2">
-              {skills?.map((skill, index) => (
-                <TzLabel key={index} label={skill} variant="secondary" />
-              ))}
-            </div>
-          )}
-        </div>
-        <div className="col-span-2 space-y-4">
-          <div className="space-y-4">
-            {achievements.map(
-              (achievement, index) =>
-                (versionAchievements == versionType.FULL || index < 2) && (
-                  <motion.p
-                    key={index}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                  >
-                    {achievement}{" "}
-                    {versionAchievements == versionType.SHORT &&
-                      index == 1 &&
-                      achievements.length > 2 && (
-                        <span
-                          className={styles.aHover}
-                          onClick={() =>
-                            setVersionAchievements(versionType.FULL)
-                          }
-                        >
-                          See more...
-                        </span>
-                      )}
-                  </motion.p>
-                )
+    <div {...props}>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <Heading level="h3" title={title} />
+        <Grid cols={3} className="mt-4 space-y-4">
+          <div className="grid grid-flow-row auto-rows-max">
+            <span>{companyName}</span>
+            <span className="italic text-c2 lg:text-c3">{location}</span>
+            <span className="font-bold py-4">{period}</span>
+            {skills && (
+              <div className="flex flex-wrap gap-2">
+                {skills?.map((skill, index) => (
+                  <TzLabel key={index} label={skill} variant="secondary" />
+                ))}
+              </div>
             )}
           </div>
-          {links && (
-            <List className="pl-4">
-              {links?.map((link, index) => (
-                <li key={index}>
-                  <a
-                    href={link.link}
-                    title={link.label}
-                    target="_blank"
-                    className={styles.aHover}
-                  >
-                    {link.label}
-                  </a>
-                </li>
-              ))}
-            </List>
-          )}
-        </div>
-      </Grid>
-    </motion.div>
+          <div className="col-span-2 space-y-4">
+            <div className="space-y-4">
+              {achievements.map(
+                (achievement, index) =>
+                  (versionAchievements == versionType.FULL || index < 2) && (
+                    <motion.p
+                      key={index}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                    >
+                      {achievement}{" "}
+                      {versionAchievements == versionType.SHORT &&
+                        index == 1 &&
+                        achievements.length > 2 && (
+                          <span
+                            className={styles.aHover}
+                            onClick={() =>
+                              setVersionAchievements(versionType.FULL)
+                            }
+                          >
+                            See more...
+                          </span>
+                        )}
+                    </motion.p>
+                  )
+              )}
+            </div>
+            {links && (
+              <List className="pl-4">
+                {links?.map((link, index) => (
+                  <li key={index}>
+                    <a
+                      href={link.link}
+                      title={link.label}
+                      target="_blank"
+                      className={styles.aHover}
+                    >
+                      {link.label}
+                    </a>
+                  </li>
+                ))}
+              </List>
+            )}
+          </div>
+        </Grid>
+      </motion.div>
+    </div>
   );
 }
