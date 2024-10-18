1f8dd797e96f2bf99d8eba2da57cdaeb62536ae8
Antonio Zanotti <antoniorzanotti@gmail.com>
2024-10-18 17:04:32 -0300

    fix: allow company to be null

--- src/domain/home/article-list/article/article-container-interface.ts
+++ src/domain/home/article-list/article/article-container-interface.ts
@@ -2,7 +2,7 @@ import { Link } from "@/types/link";
 
 export interface ArticleContainerInterface extends React.ComponentProps<"div"> {
   title: string;
-  company: string;
+  company: string | null;
   location?: string;
   period: string;
   paragraphs: string[];
